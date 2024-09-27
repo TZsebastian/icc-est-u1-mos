f99d0750c2ba4637fc7ea5d11f922d950ec7d33c
TZSebastian <telmozurita23@gmail.com>
2024-09-27 13:11:21 -0500

    Pracitca Finalizada

--- src/App.java
+++ src/App.java
@@ -2,14 +2,15 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Métodos de ordenamiento Selección");
         MetodosOrdenamiento mO = new MetodosOrdenamiento();
+
         int[] arreglo = {5, 10, 15, 12};
 
-        int[] arregloOrdenado = mO.sortBySeleccion(arreglo, true);
-        System.out.print("-");
-        mO.printArregloOrdenado(arregloOrdenado);
+        mO.sortBySeleccion(arreglo, true);
+        mO.printArregloOrdenado(arreglo);
+
         System.out.println();
+
         mO.sortBySeleccion(arreglo, false);
-        System.out.print("-");
         mO.printArregloOrdenado(arreglo);
     }
 }
--- src/MetodosOrdenamiento.java
+++ src/MetodosOrdenamiento.java
@@ -17,13 +17,6 @@ public class MetodosOrdenamiento {
         return arreglo;
     }
 
-    public void printArregloOrdenado(int[] arreglo) {
-        for (int elemento : arreglo) {
-            System.out.print(elemento + " ");
-        }
-        System.out.println();
-    }
-
     public int[] sortBySeleccion(int[] arreglo, boolean ascendente) {
         int n = arreglo.length;
 
@@ -45,5 +38,11 @@ public class MetodosOrdenamiento {
             arreglo[i] = aux;
         }
         return arreglo;
+    }
+    public void printArregloOrdenado(int[] arreglo) {
+        for (int elemento : arreglo) {
+            System.out.print(elemento + "-");
+        }
+        System.out.println();
     }
 }
